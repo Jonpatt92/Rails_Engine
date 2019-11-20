d5b496aa2d0d7ede5097d8fa0a019a8b6209cc34
Jon Patterson <JonPatt92@gmail.com>
2019-11-20 09:30:43 -0700

    Generate models and migrations

---
+++ db/migrate/20191120154645_create_invoice_items.rb
@@ -0,0 +1,12 @@
+class CreateInvoiceItems < ActiveRecord::Migration[5.1]
+  def change
+    create_table :invoice_items do |t|
+      t.string :quantity
+      t.float :unit_price
+      t.references :item, foreign_key: true
+      t.references :invoice, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
