fc2ad58479a68a9c2cb5f039a9449104a6268a58
Jon Patterson <JonPatt92@gmail.com>
2019-11-20 11:48:04 -0700

    Add notes and methods from Advanced Active Record lesson

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,4 +5,13 @@ class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :transactions
   has_many :items, through: :invoice_items
+
+  def self.largest_invoices(limit=5) # With successful transactions
+    joins(:invoice_items, :transactions)
+    .select("invoices.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS total_revenue")
+    .merge(Transaction.successful)
+    .group(:id)
+    .order('total_revenue DESC')
+    .limit(limit)
+  end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,14 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
+
+  scope :successful, -> { where(result: 'success') } # Creates a Lambda object using a '->' 'stabby lambda'.
+  # Can be called like a class method. Filters out any Transactions with a result other than 'success'
+
 end
+
+# def self.successfull
+# where(result: 'success')
+# end
+
+# default_scope { where(result: 'success') } # This will run with every query made to the Transaction model. Also applied when making a record.
+# DON'T USE DEFAULT SCOPES, they will complicate large databases and cause unexpected behavior.
