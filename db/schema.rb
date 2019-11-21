0d742ea7526162397d91d8e7c57e8b4a76bcdc58
Jon Patterson <JonPatt92@gmail.com>
2019-11-21 10:55:04 -0700

    Change Unit Price back to a float

--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20191120154740) do
 
   create_table "invoice_items", force: :cascade do |t|
     t.integer "quantity"
-    t.integer "unit_price"
+    t.float "unit_price"
     t.bigint "item_id"
     t.bigint "invoice_id"
     t.datetime "created_at", null: false
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 20191120154740) do
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.integer "unit_price"
+    t.float "unit_price"
     t.bigint "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
