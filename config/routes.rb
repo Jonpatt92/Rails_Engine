e9dfee9184cd193fef7d21e94700b743febb4663
Jon Patterson <JonPatt92@gmail.com>
2019-11-21 20:32:02 -0700

    Add routes for merchant endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+
+      namespace :merchants do
+        get '/find', to: 'find#show'
+        get '/find_all', to: 'find#index'
+        get '/:id/items', to: 'items#index'
+        get '/:id/invoices', to: 'invoices#index'
+      end
+
+      resources :merchants, only: [:index, :show]
+    end
+  end
 end
