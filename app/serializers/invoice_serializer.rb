312d14ffda96c25813133bf02de57fb0ffa599dc
Jon Patterson <JonPatt92@gmail.com>
2019-11-21 20:32:35 -0700

    Build Invoice serializer with merchant endpoints in mind

---
+++ app/serializers/invoice_serializer.rb
@@ -0,0 +1,19 @@
+class InvoiceSerializer
+  include FastJsonapi::ObjectSerializer
+
+  attributes :status
+  belongs_to :merchant
+  belongs_to :customer
+  has_many :invoice_items
+  has_many :transactions
+
+  attribute :payment_status do |object|
+    unless Transaction.where("invoice_id = ?", object.id) == []
+      if object.transactions[0][:result] == "success"
+        "paid"
+      elsif object.transactions[0][:result] == "failed"
+        "unpaid"
+      end
+    end
+  end
+end
