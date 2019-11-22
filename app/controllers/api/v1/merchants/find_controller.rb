cdfcd0d40e291c7b7556ad5a56dad3224edb595d
Jon Patterson <JonPatt92@gmail.com>
2019-11-21 20:34:24 -0700

    Build Merchant Find controller to handle query params

---
+++ app/controllers/api/v1/merchants/find_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::Merchants::FindController < ApplicationController
+# Need to make searches case insensitive
+  def show
+    render json: MerchantSerializer.new(Merchant.find_by(valid_params))
+  end
+
+  def index
+    render json: MerchantSerializer.new(Merchant.where(valid_params))
+  end
+
+  private
+
+  def valid_params
+    params.permit(:id, :name, :created_at, :updated_at)
+  end
+end
