b4dea5ea60fd8e9c2a8e8968566bfd740536ab41
Jon Patterson <JonPatt92@gmail.com>
2019-11-26 17:31:46 -0700

    Add after(:create) callback in Merchant Factory to create relational Items and Invoices

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :merchant do
-    name { "MyString" }
+    name { "Banana Stand" }
+
+    after(:create) do |merchant|
+      create_list(:item,
+                      5,
+      merchant: merchant
+      )
+    end
+
+    after(:create) do |merchant|
+      create_list(:invoice,
+                         3,
+         merchant: merchant
+      )
+    end
   end
 end
