50db8f338322101d223ad29b9841e3a60f5f4813
Jon Patterson <JonPatt92@gmail.com>
2019-11-29 13:50:44 -0700

    Add route for 'random' endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
 
       namespace :merchants do
+        get '/random', to: 'random#show'
         get '/find', to: 'find#show'
         get '/find_all', to: 'find#index'
         get '/:id/items', to: 'items#index'
