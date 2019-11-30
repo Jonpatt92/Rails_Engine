498e32c659e5b2977b701e0fa3515de099f2c1c9
Jon Patterson <JonPatt92@gmail.com>
2019-11-30 14:49:31 -0700

    Refactor factories to no longer dynamically create relational objects

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -1,19 +1,5 @@
 FactoryBot.define do
   factory :merchant do
     name { "Banana Stand" }
-
-    after(:create) do |merchant|
-      create_list(:item,
-                      5,
-      merchant: merchant
-      )
-    end
-
-    after(:create) do |merchant|
-      create_list(:invoice,
-                         3,
-         merchant: merchant
-      )
-    end
   end
 end
