06681d015b7d7bf525fc1d3a51840192629d49d9
Jon Patterson <JonPatt92@gmail.com>
2019-11-30 15:13:16 -0700

    Comment out unbuilt/untested routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,38 +18,38 @@ Rails.application.routes.draw do
         get '/:id/favorite_customer', to: 'favorite_customer#show'
       end
 
-      namespace :items do
-        ## Search ##
-        get '/find',                  to: 'find#show'
-        get '/find_all',              to: 'find#index'
-        get '/random',                to: 'random#show'
+      # namespace :items do
+      #   ## Search ##
+      #   get '/find',                  to: 'find#show'
+      #   get '/find_all',              to: 'find#index'
+      #   get '/random',                to: 'random#show'
+      #
+      #   ## Relationships ##
+      #   get '/:id/merchant',          to: 'merchant#show'
+      #   get '/:id/invoice_items',     to: 'invoice_items#index'
+      #
+      #   ## Business Logic ##
+      #   get '/most_revenue',          to: 'most_revenue#index'
+      #   get '/:id/best_day',          to: 'best_day#show'
+      # end
 
-        ## Relationships ##
-        get '/:id/merchant',          to: 'merchant#show'
-        get '/:id/invoice_items',     to: 'invoice_items#index'
-
-        ## Business Logic ##
-        get '/most_revenue',          to: 'most_revenue#index'
-        get '/:id/best_day',          to: 'best_day#show'
-      end
-
-      namespace :customers do
-        ## Search ##
-        get '/find',                  to: 'find#show'
-        get '/find_all',              to: 'find#index'
-        get '/random',                to: 'random#show'
-
-        ## Relationships ##
-        get '/:id/invoices',          to: 'invoices#index'
-        get '/:id/transactions',      to: 'transactions#index'
-
-        ## Business Logic ##
-        get '/:id/favorite_merchant', to: 'favorite_merchant#show'
-      end
+      # namespace :customers do
+      #   ## Search ##
+      #   get '/find',                  to: 'find#show'
+      #   get '/find_all',              to: 'find#index'
+      #   get '/random',                to: 'random#show'
+      #
+      #   ## Relationships ##
+      #   get '/:id/invoices',          to: 'invoices#index'
+      #   get '/:id/transactions',      to: 'transactions#index'
+      #
+      #   ## Business Logic ##
+      #   get '/:id/favorite_merchant', to: 'favorite_merchant#show'
+      # end
 
       resources :merchants, only: [:index, :show]
-      resources :items, only: [:index, :show]
-      resources :customers, only: [:index, :show]
+      # resources :items, only: [:index, :show]
+      # resources :customers, only: [:index, :show]
     end
   end
 end
