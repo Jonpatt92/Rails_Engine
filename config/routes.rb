93152e135378251732eedab8e5d7ffbe27f039f3
Jon Patterson <JonPatt92@gmail.com>
2019-11-30 15:09:54 -0700

    Organize routes and add routes for customers and items.

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,48 @@ Rails.application.routes.draw do
     namespace :v1 do
 
       namespace :merchants do
+        ## Search ##
         get '/find',                  to: 'find#show'
         get '/find_all',              to: 'find#index'
         get '/random',                to: 'random#show'
-        get '/revenue',               to: 'total_revenue#show'
-        get '/most_revenue',          to: 'most_revenue#index'
-        get '/:id/favorite_customer', to: 'customer#show'
+
+        ## Relationships ##
         get '/:id/items',             to: 'items#index'
         get '/:id/invoices',          to: 'invoices#index'
+
+        ## Business Logic ##
+        get '/revenue',               to: 'total_revenue#show'
+        get '/most_revenue',          to: 'most_revenue#index'
+        get '/:id/favorite_customer', to: 'favorite_customer#show'
+      end
+
+      namespace :items do
+        ## Search ##
+        get '/find',                  to: 'find#show'
+        get '/find_all',              to: 'find#index'
+        get '/random',                to: 'random#show'
+
+        ## Relationships ##
+        get '/:id/merchant',          to: 'merchant#show'
+        get '/:id/invoice_items',     to: 'invoice_items#index'
+
+        ## Business Logic ##
+        get '/most_revenue',          to: 'most_revenue#index'
+        get '/:id/best_day',          to: 'best_day#show'
+      end
+
+      namespace :customers do
+        ## Search ##
+        get '/find',                  to: 'find#show'
+        get '/find_all',              to: 'find#index'
+        get '/random',                to: 'random#show'
+
+        ## Relationships ##
+        get '/:id/invoices',          to: 'invoices#index'
+        get '/:id/transactions',      to: 'transactions#index'
+
+        ## Business Logic ##
+        get '/:id/favorite_merchant', to: 'favorite_merchant#show'
       end
 
       resources :merchants, only: [:index, :show]
