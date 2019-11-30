e0c2de31a51861de2b7e56c3152def38a77954bf
Jon Patterson <JonPatt92@gmail.com>
2019-11-30 16:18:36 -0700

    Build controller index action for most_revenue endpoint

--- app/controllers/api/v1/merchants/most_revenue_controller.rb
+++ app/controllers/api/v1/merchants/most_revenue_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Merchants::MostRevenueController < ApplicationController
   def index
-    render json: MostRevenueSerializer.new(Merchant.most_revenue)
+    render json: MostRevenueSerializer.new(Merchant.most_revenue(params["quantity"]))
   end
 end
