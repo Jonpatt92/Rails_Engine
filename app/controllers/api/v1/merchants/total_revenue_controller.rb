145b18f8ce43e4efbe94d41c2816c3f8b73ca85c
Jon Patterson <JonPatt92@gmail.com>
2019-11-30 16:19:17 -0700

    Add proc :if statement to eliminate date_specified attribute when no date query is given

--- app/controllers/api/v1/merchants/total_revenue_controller.rb
+++ app/controllers/api/v1/merchants/total_revenue_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::Merchants::TotalRevenueController < ApplicationController
   def show
     render json: TotalRevenueSerializer.new(
       Merchant.total_revenue(params[:date]),
-      {params: {date_specified: params[:date]}})
+      {params: {date_query: params[:date]}})
   end
 end
--- app/serializers/total_revenue_serializer.rb
+++ app/serializers/total_revenue_serializer.rb
@@ -1,8 +1,14 @@
 class TotalRevenueSerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :date_specified do |obj, params|
-    params[:date_specified]
+  # IF params[:date_query] != nil #
+  # assign an attribute called 'date_specified' #
+  # equal to the date being queried #
+  # which is passed in the param [:date_query] from the controller #
+  attribute :date_specified, if: Proc.new { |record, params|
+    params[:date_query]
+    } do |obj, params|
+    params[:date_query]
   end
 
   attributes :total_revenue do |obj|
