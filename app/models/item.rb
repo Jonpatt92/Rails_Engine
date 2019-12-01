95316dc38fb7a2771fd3df593e45c69e6dfe791b
Jon Patterson <JonPatt92@gmail.com>
2019-12-01 13:10:26 -0700

    Add transactions relationship to item. Build 'best_day' business logic query.

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
 
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+  has_many :transactions, through: :invoices
 
   after_validation :convert_to_decimal
 
@@ -14,6 +15,7 @@ class Item < ApplicationRecord
                  :description,
                    :unit_price
 
+## /most_revenue?quantity=x Returns the top x items ranked by total revenue ##
   def self.most_revenue(quantity)
     select("items.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
     .joins(invoice_items: [invoice: :transactions])
@@ -21,5 +23,18 @@ class Item < ApplicationRecord
     .group('items.id')
     .order('revenue desc')
     .limit(quantity)
+  end
+
+  ## /api/v1/items/:id/best_day returns the date with the most sales for the given item using the invoice date. ##
+  ## If there are multiple days with equal number of sales, return the most recent day. ##
+  def best_day
+    invoices
+    .joins(:transactions, :invoice_items)
+    .select("invoices.created_at::date, sum(invoice_items.quantity) AS purchases")
+    .merge(Transaction.successful)
+    .where(invoice_items: {item_id: id})
+    .group('invoices.created_at::date')
+    .order('purchases desc, invoices.created_at::date desc')
+    .first
   end
 end
