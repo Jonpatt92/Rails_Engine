5c9ee624f3db5ad7eec40c8c6e4d7896991dba0a
Jon Patterson <JonPatt92@gmail.com>
2019-11-30 18:43:17 -0700

    Build attributes for favorite_customer endpoint serializer

--- app/serializers/favorite_customer_serializer.rb
+++ app/serializers/favorite_customer_serializer.rb
@@ -1,4 +1,18 @@
 class FavoriteCustomerSerializer
   include FastJsonapi::ObjectSerializer
 
+  attributes :merchant_name do |object, params|
+    params[:merchant_name]
+  end
+
+  attributes :favorite_customer_id do |object|
+    object.id
+  end
+
+  attributes :favorite_customer_first_name, &:first_name
+  attributes :favorite_customer_last_name, &:last_name
+
+  attributes :successful_transactions do |object|
+    object.successful_transactions
+  end
 end
