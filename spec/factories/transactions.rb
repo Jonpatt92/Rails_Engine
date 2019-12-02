5214c747147221f77c12b1c8d20a54ceffd964aa
Jon Patterson <JonPatt92@gmail.com>
2019-12-01 20:05:34 -0700

    Adjust transaction factory values

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :transaction do
-    credit_card_number { 123456789 }
-    credit_card_expiration_date { 610 }
+    credit_card_number { 1234 }
+    credit_card_expiration_date { 123 }
     result { "success" }
     invoice
   end
