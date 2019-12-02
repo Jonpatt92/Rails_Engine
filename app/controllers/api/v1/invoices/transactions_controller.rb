0a06622fc7cace2ee2a95975f6e032346068546d
Jon Patterson <JonPatt92@gmail.com>
2019-12-01 18:23:30 -0700

    Build Invoice transaction controller for relational endpoint

---
+++ app/controllers/api/v1/invoices/transactions_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::Invoices::TransactionsController < ApplicationController
+  def index
+    render json: TransactionSerializer.new(Transaction.where("invoice_id = ?", params[:id]))
+  end
+end
