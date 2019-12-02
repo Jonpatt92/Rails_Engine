394fff3ac6f26ae1b55843aa90beb74baafeac36
Jon Patterson <JonPatt92@gmail.com>
2019-12-01 18:23:53 -0700

    Build Invoice invoice_item controller for relational endpoint

---
+++ app/controllers/api/v1/invoices/invoice_items_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::Invoices::InvoiceItemsController < ApplicationController
+  def index
+    render json: InvoiceItemSerializer.new(InvoiceItem.where("invoice_id = ?", params[:id]))
+  end
+end
