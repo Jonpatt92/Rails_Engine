b180679ec92f21e39f73b14f4c34444554f11ed0
Jon Patterson <JonPatt92@gmail.com>
2019-12-29 14:24:14 -0700

    Replace id: with item_id: to correctly represent query parameters.

--- app/controllers/api/v1/items/best_day_controller.rb
+++ app/controllers/api/v1/items/best_day_controller.rb
@@ -1,7 +1,5 @@
 class Api::V1::Items::BestDayController < ApplicationController
   def show
-    # item = Item.find(params[:id])
-    render json: BestDaySerializer.new(Item.best_day(id: params[:id]))
-    binding.pry
+    render json: BestDaySerializer.new(Item.best_day(item_id: params[:id]))
   end
 end
